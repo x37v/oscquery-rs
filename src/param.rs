c44d246286970e7d16dbd0e5ec1d590f24b303b1
Alex Norman <xnor@perfectco.com>
2020-04-26 15:49:47 -0700

    TODO some stuff

--- src/param.rs
+++ src/param.rs
@@ -5,16 +5,16 @@ pub enum ParamGet {
     Int(ValueGet<i32>),
     Float(ValueGet<f32>),
     String(ValueGet<String>),
-    Blob(ValueGet<Box<[u8]>>), //does clip mode make and range make sense?
     Time(ValueGet<(u32, u32)>),
     Long(ValueGet<i64>),
     Double(ValueGet<f64>),
     Char(ValueGet<char>),
     Midi(ValueGet<(u8, u8, u8, u8)>),
     Bool(ValueGet<bool>),
-    Array(Box<[Self]>),
-    Nil,
-    Inf,
+    //TODO Blob(ValueGet<Box<[u8]>>), //does clip mode make and range make sense?
+    //TODO Array(Box<[Self]>),
+    //TODO Nil,
+    //TODO Inf,
 }
 
 #[derive(Debug)]
@@ -22,14 +22,14 @@ pub enum ParamSet {
     Int(ValueSet<i32>),
     Float(ValueSet<f32>),
     String(ValueSet<String>),
-    Blob(ValueSet<Box<[u8]>>), //does clip mode make and range make sense?
     Time(ValueSet<(u32, u32)>),
     Long(ValueSet<i64>),
     Double(ValueSet<f64>),
     Char(ValueSet<char>),
     Midi(ValueSet<(u8, u8, u8, u8)>),
     Bool(ValueSet<bool>),
-    Array(Box<[Self]>),
+    //TODO Blob(ValueSet<Box<[u8]>>), //does clip mode make and range make sense?
+    //TODO Array(Box<[Self]>),
 }
 
 #[derive(Debug)]
@@ -37,12 +37,12 @@ pub enum ParamGetSet {
     Int(ValueGetSet<i32>),
     Float(ValueGetSet<f32>),
     String(ValueGetSet<String>),
-    Blob(ValueGetSet<Box<[u8]>>), //does clip mode make and range make sense?
     Time(ValueGetSet<(u32, u32)>),
     Long(ValueGetSet<i64>),
     Double(ValueGetSet<f64>),
     Char(ValueGetSet<char>),
     Midi(ValueGetSet<(u8, u8, u8, u8)>),
     Bool(ValueGetSet<bool>),
-    Array(Box<[Self]>),
+    //TODO Blob(ValueGetSet<Box<[u8]>>), //does clip mode make and range make sense?
+    //TODO Array(Box<[Self]>),
 }
