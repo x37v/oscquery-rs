7aed3f1dbbece47dc6c9ae2656cddb7e216db437
Alex Norman <xnor@perfectco.com>
2020-04-28 20:41:57 -0700

    add some detail

--- examples/server.rs
+++ examples/server.rs
@@ -1,10 +1,25 @@
+use ::atomic::Atomic;
+use oscquery::param::*;
 use oscquery::root::Root;
 use oscquery::service::ServiceHandle;
+use oscquery::value::*;
 use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
     let root = Arc::new(Root::new());
+    let c = oscquery::node::Container::new("foo".into(), Some("description of foo".into()));
+    assert!(c.is_ok());
+    let res = root.add_node(c.unwrap().into(), None);
+    assert!(res.is_ok());
+
+    let a = Arc::new(Atomic::new(2084i32));
+    let v = ParamGet::Int(ValueBuilder::new(a.clone() as _).build());
+    let v = vec![v];
+    let m = oscquery::node::Get::new("bar".into(), None, v.into_iter());
+
+    let res = root.add_node(m.unwrap().into(), Some(res.unwrap()));
+    assert!(res.is_ok());
     let _handle = ServiceHandle::new(root);
     loop {
         std::thread::sleep(std::time::Duration::from_millis(10));
