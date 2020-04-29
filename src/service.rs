898793046bcf5cac27aeba3bb30adfd266956aab
Alex Norman <xnor@perfectco.com>
2020-04-29 07:58:46 -0700

    moved into async, doesn't need tokio main to call

--- src/service.rs
+++ src/service.rs
@@ -11,38 +11,39 @@ pub struct ServiceHandle {
 impl ServiceHandle {
     pub fn new(root: Arc<Root>) -> Self {
         let root = root.clone();
-        let make_service = make_service_fn(move |_| {
-            let root = root.clone();
-            async move {
-                Ok::<_, Error>(service_fn(move |_req| {
-                    let root = root.clone();
-                    async move {
-                        let s = serde_json::to_value(root).unwrap().to_string();
-                        Ok::<_, Error>(Response::new(Body::from(s)))
-                    }
-                }))
-            }
-        });
-        let server = Server::bind(&([127, 0, 0, 1], 3000).into()).serve(make_service);
-
         let (tx, rx) = tokio::sync::oneshot::channel::<()>();
-        let graceful = server.with_graceful_shutdown(async {
-            rx.await.ok();
-        });
-
         std::thread::spawn(move || {
             let mut rt = tokio::runtime::Builder::new()
+                .basic_scheduler()
                 .threaded_scheduler()
                 .enable_all()
                 .build()
-                .unwrap();
+                .expect("could not create runtime");
             rt.block_on(async {
+                let make_service = make_service_fn(move |_| {
+                    let root = root.clone();
+                    async move {
+                        Ok::<_, Error>(service_fn(move |_req| {
+                            let root = root.clone();
+                            async move {
+                                let s = serde_json::to_value(root).unwrap().to_string();
+                                Ok::<_, Error>(Response::new(Body::from(s)))
+                            }
+                        }))
+                    }
+                });
+                let server = Server::bind(&([127, 0, 0, 1], 3000).into()).serve(make_service);
+
+                let graceful = server.with_graceful_shutdown(async {
+                    rx.await.ok();
+                    println!("quitting");
+                });
+
                 if let Err(e) = graceful.await {
                     eprintln!("server error: {}", e);
                 }
             });
         });
-
         Self { tx: Some(tx) }
     }
 }
