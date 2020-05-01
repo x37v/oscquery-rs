5a3ac5dee6b85a976ec636448be36b3c4e4cb98e
Alex Norman <xnor@perfectco.com>
2020-04-30 20:01:13 -0700

    using a struct

--- src/service.rs
+++ src/service.rs
@@ -1,11 +1,63 @@
 use crate::root::Root;
 
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Error, Response, Server};
+use futures::future;
+use hyper::service::{make_service_fn, service_fn, Service};
+use hyper::{Body, Error, Request, Response, Server};
 use std::sync::Arc;
+use std::task::{Context, Poll};
 
 pub struct ServiceHandle {
     tx: Option<tokio::sync::oneshot::Sender<()>>,
+}
+
+struct Svc {
+    root: Arc<Root>,
+}
+
+struct MakeSvc {
+    root: Arc<Root>,
+}
+
+impl Service<Request<Body>> for Svc {
+    type Response = Response<Body>;
+    type Error = hyper::Error;
+    type Future = future::Ready<Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Ok(()).into()
+    }
+
+    fn call(&mut self, req: Request<Body>) -> Self::Future {
+        let rsp = Response::builder();
+
+        let uri = req.uri();
+        if uri.path() != "/" {
+            let body = Body::from(Vec::new());
+            let rsp = rsp.status(404).body(body).unwrap();
+            return future::ok(rsp);
+        }
+
+        let s = serde_json::to_value(self.root.clone()).unwrap().to_string();
+        let body = Body::from(s);
+        let rsp = rsp.status(200).body(body).unwrap();
+        future::ok(rsp)
+    }
+}
+
+impl<T> Service<T> for MakeSvc {
+    type Response = Svc;
+    type Error = std::io::Error;
+    type Future = future::Ready<Result<Self::Response, Self::Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Ok(()).into()
+    }
+
+    fn call(&mut self, _: T) -> Self::Future {
+        future::ok(Svc {
+            root: self.root.clone(),
+        })
+    }
 }
 
 impl ServiceHandle {
@@ -20,19 +72,7 @@ impl ServiceHandle {
                 .build()
                 .expect("could not create runtime");
             rt.block_on(async {
-                let make_service = make_service_fn(move |_| {
-                    let root = root.clone();
-                    async move {
-                        Ok::<_, Error>(service_fn(move |_req| {
-                            let root = root.clone();
-                            async move {
-                                let s = serde_json::to_value(root).unwrap().to_string();
-                                Ok::<_, Error>(Response::new(Body::from(s)))
-                            }
-                        }))
-                    }
-                });
-                let server = Server::bind(&([127, 0, 0, 1], port).into()).serve(make_service);
+                let server = Server::bind(&([127, 0, 0, 1], port).into()).serve(MakeSvc { root });
 
                 let graceful = server.with_graceful_shutdown(async {
                     rx.await.ok();
