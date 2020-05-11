9d060296241c467341671370319235daf2e284f2
Alex Norman <alex@x37v.info>
2020-05-11 08:09:42 -0700

    use TCP stream read timeout

--- src/service/websocket.rs
+++ src/service/websocket.rs
@@ -3,11 +3,15 @@ use std::thread::{spawn, JoinHandle};
 
 use serde::{Deserialize, Serialize};
 
-use tungstenite::{accept, Message};
+use tungstenite::{accept, error::Error, Message};
 
 use crate::root::RootInner;
 use std::sync::Arc;
 use std::sync::RwLock;
+use std::time::Duration;
+
+//what we set the TCP stream read timeout to
+const READ_TIMEOUT: Duration = Duration::from_millis(1);
 
 pub struct WSService {
     handle: Option<JoinHandle<()>>,
@@ -46,34 +50,57 @@ impl WSService {
         let local_addr = server.local_addr()?;
         let handle = spawn(move || {
             for stream in server.incoming() {
+                if stream.is_err() {
+                    continue;
+                }
+                let stream = stream.unwrap();
+                stream
+                    .set_read_timeout(Some(READ_TIMEOUT))
+                    .expect("cannot set read timeout");
                 let root = root.clone();
                 spawn(move || {
-                    if let Ok(mut websocket) = accept(stream.unwrap()) {
+                    if let Ok(mut websocket) = accept(stream) {
                         loop {
-                            if let Ok(msg) = websocket.read_message() {
-                                match msg {
-                                    //binary messages are OSC packets
-                                    Message::Binary(v) => {
-                                        if let Ok(packet) = rosc::decoder::decode(&v) {
-                                            if let Ok(root) = root.read() {
-                                                root.handle_osc_packet(&packet, None, None);
+                            match websocket.read_message() {
+                                Ok(msg) => {
+                                    match msg {
+                                        //binary messages are OSC packets
+                                        Message::Binary(v) => {
+                                            if let Ok(packet) = rosc::decoder::decode(&v) {
+                                                if let Ok(root) = root.read() {
+                                                    root.handle_osc_packet(&packet, None, None);
+                                                }
                                             }
                                         }
-                                    }
-                                    Message::Text(s) => {
-                                        if let Ok(cmd) = serde_json::from_str::<
-                                            WSCommandPacket<ClientServerCmd>,
-                                        >(
-                                            &s
-                                        ) {
-                                            println!("got command {:?}", cmd);
+                                        Message::Text(s) => {
+                                            if let Ok(cmd) =
+                                                serde_json::from_str::<
+                                                    WSCommandPacket<ClientServerCmd>,
+                                                >(&s)
+                                            {
+                                                println!("got command {:?}", cmd);
+                                            }
                                         }
+                                        Message::Close(..) => return,
+                                        Message::Ping(d) => {
+                                            //TODO if err, return?
+                                            let _ = websocket.write_message(Message::Pong(d));
+                                        }
+                                        Message::Pong(..) => println!("pong"),
+                                    };
+                                }
+                                Err(Error::ConnectionClosed) | Err(Error::AlreadyClosed) => {
+                                    return;
+                                }
+                                Err(Error::Io(e)) => match e.kind() {
+                                    std::io::ErrorKind::WouldBlock
+                                    | std::io::ErrorKind::TimedOut => (),
+                                    e @ _ => {
+                                        println!("io ErrorKind {:?}", e);
+                                        return;
                                     }
-                                    Message::Close(..) => return,
-                                    Message::Ping(..) | Message::Pong(..) => (),
-                                };
-                            } else {
-                                return;
+                                },
+                                Err(..) => (), //TODO
                             }
                         }
                     }
