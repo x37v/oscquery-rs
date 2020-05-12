32fbaa5127d079fa0389b05a3bb25cc4677eb5df
Alex Norman <alex@x37v.info>
2020-05-11 19:42:49 -0700

    listen and ignore

--- examples/send.rs
+++ examples/send.rs
@@ -9,24 +9,32 @@ fn main() {
     println!("Connected to the server");
     println!("Response HTTP code: {}", response.status());
     println!("Response contains the following headers:");
+
     for (ref header, _value) in response.headers() {
         println!("* {}", header);
     }
 
     socket
         .write_message(Message::Text(
-            "{\"COMMAND\":\"LISTEN\",\"DATA\":\"awy\"}".into(),
+            "{\"COMMAND\":\"IGNORE\",\"DATA\":\"/foo/bar\"}".into(),
         ))
         .unwrap();
 
-    let v = vec![OscType::Int(22)];
+    socket
+        .write_message(Message::Text(
+            "{\"COMMAND\":\"LISTEN\",\"DATA\":\"/foo/bar\"}".into(),
+        ))
+        .unwrap();
+
+    let v = vec![OscType::Int(101)];
     let buf = rosc::encoder::encode(&OscPacket::Message(OscMessage {
         addr: "/foo/bar".to_string(),
         args: v,
     }))
     .unwrap();
     socket.write_message(Message::Binary(buf)).unwrap();
-    //let msg = socket.read_message().expect("Error reading message");
-    //println!("Received: {}", msg);
-    // socket.close(None);
+
+    let msg = socket.read_message().expect("Error reading message");
+    println!("Received: {}", msg);
+    socket.close(None);
 }
