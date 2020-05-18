4710dacccf9d5b9ec9a71d2fb677db4801524a9e
Alex Norman <alex@x37v.info>
2020-05-18 11:37:02 -0700

    pooorly named but there

--- examples/send.rs
+++ examples/send.rs
@@ -3,7 +3,7 @@ use url::Url;
 
 fn main() {
     let (mut socket, response) =
-        connect(Url::parse("ws://localhost:5678/socket").unwrap()).expect("Can't connect");
+        connect(Url::parse("ws://localhost:3002/socket").unwrap()).expect("Can't connect");
 
     println!("Connected to the server");
     println!("Response HTTP code: {}", response.status());
@@ -15,8 +15,13 @@ fn main() {
 
     socket
         .write_message(Message::Text(
-            "{\"COMMAND\":\"IGNORE\",\"DATA\":\"/foo/bar\"}".into(),
+            "{\"COMMAND\":\"LISTEN\",\"DATA\":\"/foo/bar\"}".into(),
         ))
         .unwrap();
+
+    loop {
+        let r = socket.read_message();
+        println!("{:?}", r);
+    }
     socket.close(None);
 }
