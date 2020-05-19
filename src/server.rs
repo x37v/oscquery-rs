41e89b98cb60bd2291f6824be395edd73b68e574
Alex Norman <alex@x37v.info>
2020-05-18 18:57:05 -0700

    forward more methods, doc

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,6 @@
-use crate::root::Root;
-use crate::service::*;
+use crate::node::Node;
+use crate::root::{NodeHandle, Root};
+use crate::service::{http, osc, websocket};
 use std::net::{SocketAddr, ToSocketAddrs};
 use std::sync::Arc;
 
@@ -8,7 +9,7 @@ pub struct OscQueryServer {
     root: Arc<Root>,
     osc: osc::OscService,
     ws: websocket::WSService,
-    http: http::HttpService,
+    _http: http::HttpService,
 }
 
 impl OscQueryServer {
@@ -32,7 +33,57 @@ impl OscQueryServer {
             root,
             osc,
             ws,
-            http,
+            _http: http,
         })
+    }
+
+    ///Add node to the graph at the root or as a child of the given parent
+    pub fn add_node(
+        &self,
+        node: Node,
+        parent: Option<NodeHandle>,
+    ) -> Result<NodeHandle, (Node, &'static str)> {
+        self.root.add_node(node, parent)
+    }
+
+    ///Remove the node at the handle returns it and any children if found.
+    ///
+    ///Leaves come first in returned vector.
+    pub fn rm_node(&self, handle: NodeHandle) -> Result<Vec<Node>, (NodeHandle, &'static str)> {
+        self.root.rm_node(handle)
+    }
+
+    ///Get the OSC service's bound address.
+    pub fn osc_local_addr(&self) -> &SocketAddr {
+        self.osc.local_addr()
+    }
+
+    ///Get the websocket service's bound address.
+    pub fn ws_local_addr(&self) -> &SocketAddr {
+        self.ws.local_addr()
+    }
+
+    ///Trigger a send (if possible) for the node at the given handle.
+    ///
+    ///Returns true if there was a node at the handle that could be and was triggered.
+    pub fn trigger(&self, handle: NodeHandle) -> bool {
+        if let Some(msg) = self.osc.trigger(handle) {
+            self.ws.send(msg);
+            true
+        } else {
+            false
+        }
+    }
+
+    ///Trigger a send (if possible) for the node at the given path.
+    ///
+    ///Returns true if there was a node at the path that could be and was triggered.
+    pub fn trigger_path(&self, path: &str) -> bool {
+        if let Some(msg) = self.osc.trigger_path(path) {
+            self.ws.send(msg);
+            true
+        } else {
+            false
+        }
     }
 }
