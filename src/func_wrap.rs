c156e495142854f7f404f24e79e06f82a3418973
Alex Norman <alex@x37v.info>
2020-06-01 17:25:40 -0700

    func wrappers, impl update methods

---
+++ src/func_wrap.rs
@@ -0,0 +1,37 @@
+use crate::node::OscUpdate;
+use crate::root::OscWriteCallback;
+
+use rosc::OscType;
+use std::net::SocketAddr;
+
+pub struct UpdateFunc<F>(pub F);
+pub struct UpdateFunc2<F>(pub F);
+
+impl<F> OscUpdate for UpdateFunc<F>
+where
+    F: Fn(&Vec<OscType>, Option<SocketAddr>, Option<(u32, u32)>) -> (),
+{
+    fn osc_update(
+        &self,
+        args: &Vec<OscType>,
+        addr: Option<SocketAddr>,
+        time: Option<(u32, u32)>,
+    ) -> Option<OscWriteCallback> {
+        (self.0)(args, addr, time);
+        None
+    }
+}
+
+impl<F> OscUpdate for UpdateFunc2<F>
+where
+    F: Fn(&Vec<OscType>, Option<SocketAddr>, Option<(u32, u32)>) -> Option<OscWriteCallback>,
+{
+    fn osc_update(
+        &self,
+        args: &Vec<OscType>,
+        addr: Option<SocketAddr>,
+        time: Option<(u32, u32)>,
+    ) -> Option<OscWriteCallback> {
+        (self.0)(args, addr, time)
+    }
+}
