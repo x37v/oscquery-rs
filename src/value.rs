cef5e39fcf09451ed137e1e706a525ae33b8caae
Alex Norman <alex@x37v.info>
2021-01-01 17:02:40 -0800

    impl get for box and arc

--- src/value.rs
+++ src/value.rs
@@ -1,7 +1,6 @@
 //! Parameter values and their attributes.
 use serde::{ser::SerializeMap, Deserialize, Serialize, Serializer};
-use std::fmt;
-use std::sync::Arc;
+use std::{fmt, ops::Deref, sync::Arc};
 
 mod atomic;
 mod dummy;
@@ -217,6 +216,31 @@ impl Get<String> for &str {
         self.to_string()
     }
 }
+
+macro_rules! impl_box_arc_get {
+    ($t:ty) => {
+        impl Get<$t> for Box<$t> {
+            fn get(&self) -> $t {
+                self.deref().clone()
+            }
+        }
+        impl Get<$t> for Arc<$t> {
+            fn get(&self) -> $t {
+                self.deref().clone()
+            }
+        }
+    };
+}
+
+impl_box_arc_get!(i32);
+impl_box_arc_get!(f32);
+impl_box_arc_get!(String);
+impl_box_arc_get!((u32, u32));
+impl_box_arc_get!(i64);
+impl_box_arc_get!(f64);
+impl_box_arc_get!(char);
+impl_box_arc_get!((u8, u8, u8, u8));
+impl_box_arc_get!(bool);
 
 #[cfg(test)]
 mod tests {
